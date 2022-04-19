86e5051bd0f0ac8b6542064a32b87fa7bdc8e0fc
Thomas Geissl <thomas.geissl@gmail.com>
2022-04-20 00:23:35 +0200

    added nodemon

--- io/node-crank/index.js
+++ io/node-crank/index.js
@@ -1,23 +1,24 @@
 const Server = require("node-osc").Server;
 const easymidi = require("easymidi");
+
 const portName = "IAC Driver Bus 1";
+const oscPort = 9001;
+const controller = 1;
+const channel = 1;
+
 const output = new easymidi.Output(portName);
 
-output.send("noteon", {
-  note: 64,
-  velocity: 127,
-  channel: 3,
-});
-
-var oscServer = new Server(9001, "0.0.0.0", () => {
+var oscServer = new Server(oscPort, "127.0.0.1", () => {
   console.log("OSC Server is listening");
 });
 
 oscServer.on("message", function (msg) {
   console.log(`Message: ${msg}`);
+  console.log(msg.split(","));
+  //   TODO: map value
   output.send("cc", {
-    controller: 1,
+    controller,
     value: 1,
-    channel: 1,
+    channel,
   });
 });
